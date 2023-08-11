e7379a0cdd1b04df7ef77edba792641e6b114096
Bobbi Towers <btowers793@gmail.com>
2023-08-10 17:48:08 -0700

    create env

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,3 +1,5 @@
+import { Env } from './env.js'
+
 // read
 function READ(str) {
     return str;
@@ -13,4 +15,5 @@ function PRINT(exp) {
     return exp;
 }
 
+export var repl_env = new Env();
 export const evalString = function (str) { return PRINT(EVAL(READ(str), repl_env)); };
