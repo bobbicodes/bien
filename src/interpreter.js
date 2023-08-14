7f56e54cb9aad5848386fce0911d8f31fd18004d
Bobbi Towers <btowers793@gmail.com>
2023-08-13 18:18:08 -0700

    implement loop/recur special forms

--- src/interpreter.js
+++ src/interpreter.js
@@ -77,129 +77,150 @@ export var deftests = []
 var arglist
 var fnBody
 var isMultiArity
+var loopVars = []
+// We need to store the ast so we can
+// pass it to recur later
+var loopAST = []
+var loop_env = new Env(repl_env)
 
 function _EVAL(ast, env) {
-    while (true) {
+    while(true) {
+    //printer.println("EVAL:", printer._pr_str(ast, true));
+    if (!types._list_Q(ast)) {
+        return eval_ast(ast, env);
+    }
 
-        //printer.println("EVAL:", printer._pr_str(ast, true));
-        if (!types._list_Q(ast)) {
-            return eval_ast(ast, env);
-        }
+    // apply list
+    ast = macroexpand(ast, env);
+    if (!types._list_Q(ast)) {
+        return eval_ast(ast, env);
+    }
+    if (ast.length === 0) {
+        return ast;
+    }
 
-        // apply list
-        ast = macroexpand(ast, env);
-        if (!types._list_Q(ast)) {
-            return eval_ast(ast, env);
-        }
-        if (ast.length === 0) {
-            return ast;
-        }
-
-        var a0 = ast[0], a1 = ast[1], a2 = ast[2], a3 = ast[3];
-        // Keyword functions:
-        // If the first element is a keyword,
-        // it looks up its value in its argument
-        if (types._keyword_Q(a0)) {
-            return EVAL([types._symbol("get"), a1, a0], env)
-        }
-        switch (a0.value) {
-            case "ns":
-            case "discard":
-                return null
-                return null
-            case "dispatch":
-                //console.log("eval dispatch")
-                // Regex
-                if (types._string_Q(a1)) {
-                    const re = new RegExp(a1, 'g')
-                    return re
-                }
-                // Anonymous function shorthand
-                if (types._list_Q(a1)) {
-                    let fun = [types._symbol('fn')]
-                    const args = ast.toString().match(/%\d?/g).map(types._symbol)
-                    //console.log("args:", args)
-                    let body = ast.slice(1)[0]
-                    fun.push(args)
-                    fun.push(body)
-                    var lambda = types._function(EVAL, Env, body, env, args);
-                    lambda.lambda = true
-                    return lambda
-                }
-            case "def":
-                var res = EVAL(a2, env);
-                env.set(a1, res);
-                return "#'" + a1
-            case 'deftest':
-                var res = ast.slice(2).map((x) => EVAL(x, env))
-                env.set(a1, res);
-                deftests.push({ test: a1, result: res })
-                return res
-            case 'testing':
-                return EVAL(a2, env)
-            case "let":
-                var let_env = new Env(env);
-                for (var i = 0; i < a1.length; i += 2) {
-                    let_env.set(a1[i], EVAL(a1[i + 1], let_env));
-                }
-                ast = a2;
-                env = let_env;
-                break;
-            case "quote":
-                return a1;
-            case "quasiquoteexpand":
-                return quasiquote(a1);
-            case "quasiquote":
-                ast = quasiquote(a1);
-                break;
-            case 'defmacro':
-                var func = types._clone(EVAL(a2, env));
-                func._ismacro_ = true;
-                return env.set(a1, func);
-            case 'macroexpand':
-                return macroexpand(a1, env);
-            case "try":
-                try {
-                    return EVAL(a1, env);
-                } catch (exc) {
-                    if (a2 && a2[0].value === "catch") {
-                        if (exc instanceof Error) { exc = exc.message; }
-                        return EVAL(a2[2], new Env(env, [a2[1]], [exc]));
-                    } else {
-                        throw exc;
-                    }
-                }
-            case "do":
-                eval_ast(ast.slice(1, -1), env);
-                ast = ast[ast.length - 1];
-                break;
-            case "if":
-                var cond = EVAL(a1, env);
-                if (cond === null || cond === false) {
-                    ast = (typeof a3 !== "undefined") ? a3 : null;
-                } else {
-                    ast = a2;
-                }
-                break;
-            case "fn":
-                //console.log("defining fn", ast)
-                var lambda = types._function(EVAL, Env, a2, env, a1);
+    var a0 = ast[0], a1 = ast[1], a2 = ast[2], a3 = ast[3];
+    // Keyword functions:
+    // If the first element is a keyword,
+    // it looks up its value in its argument
+    if (types._keyword_Q(a0)) {
+        return EVAL([types._symbol("get"), a1, a0], env)
+    }
+    switch (a0.value) {
+        case "ns":
+        case "discard":
+            return null
+        case "dispatch":
+            //console.log("eval dispatch")
+            // Regex
+            if (types._string_Q(a1)) {
+                const re = new RegExp(a1, 'g')
+                return re
+            }
+            // Anonymous function shorthand
+            if (types._list_Q(a1)) {
+                let fun = [types._symbol('fn')]
+                const args = ast.toString().match(/%\d?/g).map(types._symbol)
+                //console.log("args:", args)
+                let body = ast.slice(1)[0]
+                fun.push(args)
+                fun.push(body)
+                var lambda = types._function(EVAL, Env, body, env, args);
                 lambda.lambda = true
                 return lambda
-            default:
-                //console.log("calling", ast[0], env)
-                var el = eval_ast(ast, env), f = el[0];
-                if (f.lambda) {
-                    //console.log("fn is a lambda", f.__ast__)
-                }
-                if (f.__ast__) {
-                    ast = f.__ast__;
-                    env = f.__gen_env__(el.slice(1));
+            }
+        case "def":
+            var res = EVAL(a2, env);
+            env.set(a1, res);
+            return "#'" + a1
+        case "loop":
+            loopVars = []
+            loop_env = new Env(env)
+            loopAST = ast.slice(2)
+            for (var i = 0; i < a1.length; i += 2) {
+                loop_env.set(a1[i], EVAL(a1[i + 1], loop_env))
+                loopVars.push(a1[i])
+            }
+            ast = a2;
+            env = loop_env;
+            break;
+        case "recur":
+            const savedAST = eval_ast(ast.slice(1), loop_env)
+            for (var i = 0; i < loopVars.length; i += 1) {
+                loop_env.set(loopVars[i], savedAST[i]);
+            }
+            ast = loopAST[0]
+            break;
+        case 'deftest':
+            var res = ast.slice(2).map((x) => EVAL(x, env))
+            env.set(a1, res);
+            deftests.push({ test: a1, result: res })
+            return res
+        case 'testing':
+            return EVAL(a2, env)
+        case "let":
+            var let_env = new Env(env);
+            for (var i = 0; i < a1.length; i += 2) {
+                let_env.set(a1[i], EVAL(a1[i + 1], let_env));
+            }
+            ast = a2;
+            env = let_env;
+            break;
+        case "quote":
+            return a1;
+        case "quasiquoteexpand":
+            return quasiquote(a1);
+        case "quasiquote":
+            ast = quasiquote(a1);
+            break;
+        case 'defmacro':
+            var func = types._clone(EVAL(a2, env));
+            func._ismacro_ = true;
+            return env.set(a1, func);
+        case 'macroexpand':
+            return macroexpand(a1, env);
+        case "try":
+            try {
+                return EVAL(a1, env);
+            } catch (exc) {
+                if (a2 && a2[0].value === "catch") {
+                    if (exc instanceof Error) { exc = exc.message; }
+                    return EVAL(a2[2], new Env(env, [a2[1]], [exc]));
                 } else {
-                    return f.apply(f, el.slice(1));
+                    throw exc;
                 }
+            }
+        case "do":
+            eval_ast(ast.slice(1, -1), env);
+            ast = ast[ast.length - 1];
+            break;
+        case "if":
+            var cond = EVAL(a1, env);
+            if (cond === null || cond === false) {
+                ast = (typeof a3 !== "undefined") ? a3 : null;
+            } else {
+                ast = a2;
+            }
+            break;
+        case "fn":
+            //console.log("defining fn", ast)
+            var lambda = types._function(EVAL, Env, a2, env, a1);
+            lambda.lambda = true
+            return lambda
+        default:
+            //console.log("calling", ast[0], env)
+            var el = eval_ast(ast, env), f = el[0];
+            if (f.lambda) {
+                //console.log("fn is a lambda", f.__ast__)
+            }
+            if (f.__ast__) {
+                ast = f.__ast__;
+                env = f.__gen_env__(el.slice(1));
+            } else {
+                var res = f.apply(f, el.slice(1));
+                return res
+            }
         }
-
     }
 }
 
