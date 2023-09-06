f53b5a538f7f8f59ec8fdda8f542fd5ff1e73362
Bobbi Towers <btowers793@gmail.com>
2023-09-05 20:36:22 -0700

    all map arities return lists

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -47,17 +47,17 @@
                  (conj res (if (keyword? f) (get (first s) f) (f (first s))))))))
    ([f c1 c2]
     (loop [s1 (seq c1) s2 (seq c2) res []]
-      (if (or (empty? s1) (empty? s2)) res
+      (if (or (empty? s1) (empty? s2)) (apply list res)
           (recur (rest s1) (rest s2)
                  (conj res (f (first s1) (first s2)))))))
    ([f c1 c2 c3]
     (loop [s1 (seq c1) s2 (seq c2) s3 (seq c3) res []]
-      (if (or (empty? s1) (empty? s2) (empty? s3)) res
+      (if (or (empty? s1) (empty? s2) (empty? s3)) (apply list res)
           (recur (rest s1) (rest s2) (rest s3)
                  (conj res (f (first s1) (first s2) (first s3)))))))
    ([f c1 c2 c3 c4]
     (loop [s1 (seq c1) s2 (seq c2) s3 (seq c3) s4 (seq c4) res []]
-      (if (or (empty? s1) (empty? s2) (empty? s3) (empty? s4)) res
+      (if (or (empty? s1) (empty? s2) (empty? s3) (empty? s4)) (apply list res)
           (recur (rest s1) (rest s2) (rest s3) (rest s4)
                  (conj res (f (first s1) (first s2) (first s3) (first s4)))))))))
 
